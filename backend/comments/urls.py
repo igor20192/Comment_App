c762e51e930f19a35481b20aa77be4ba5cf5ce7f
igor20192 <igor.udovenko2012@meta.ua>
2024-12-12 11:30:36 +0200

    comment_app urls.py add captcha

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CommentViewSet
+from .views import CommentViewSet, get_captcha
 
 router = DefaultRouter()
 router.register(r"comments", CommentViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("captcha/", get_captcha, name="captcha"),
 ]
