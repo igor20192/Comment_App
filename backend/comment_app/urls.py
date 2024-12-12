e2fffa58b11a64aa3e66ec4c24e37f912604bb95
igor20192 <igor.udovenko2012@meta.ua>
2024-12-12 11:22:37 +0200

    comments  urls.py add path captcha

--- backend/comment_app/urls.py
+++ backend/comment_app/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("comments.urls")),
+    path("captcha/", include("captcha.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
