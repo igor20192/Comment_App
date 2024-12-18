7ddec2387218f154695b995f762cc6e045fdf6f0
igor20192 <igor.udovenko2012@meta.ua>
2024-12-18 17:33:33 +0200

    Home.js str:24 delete static add static_api

--- comment-frontend/src/components/Home.js
+++ comment-frontend/src/components/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
 
                 <Grid item xs={12} md={6}>
                     <img
-                        src={`${process.env.REACT_APP_BASE_URL}/static/images/balloons-874837_1280.jpg`}
+                        src={`${process.env.REACT_APP_BASE_URL}/static_api/images/balloons-874837_1280.jpg`}
                         alt="Comments Illustration"
                         style={{ width: "100%", borderRadius: "8px" }}
                     />
