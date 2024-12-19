f66129e0288600df870552b8ee926a0122af1313
igor20192 <igor.udovenko2012@meta.ua>
2024-12-19 17:25:27 +0200

    init-letsencrypt.sh edit domians str:8

--- comment-frontend/init-letsencrypt.sh
+++ comment-frontend/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=( comments.pp.ua www. comments.pp.ua)
+domains=(comments.pp.ua www.comments.pp.ua)
 rsa_key_size=4096
 data_path="./comment-frontend/nginx/certbot"
 email="igor.udovenko2012@meta.ua" # Adding a valid address is strongly recommended
